6565bec2477d384bdd498ed89e4065772a5111b3
p1v4aemvg <p1v4aemvg@gmail.com>
2017-10-15 19:03:43 +0300

    minor

--- src/main/java/com/by/movx/entity/FilmLang.java
+++ src/main/java/com/by/movx/entity/FilmLang.java
@@ -73,6 +73,6 @@ public class FilmLang {
     }
 
     public enum Lang {
-        NO, RU, EN, BY, DE, FR, UA, GR, LT, LV, ES, SP, IT, PL, FN, CZ
+        NO, RU, EN, BY, DE, FR, UA, GR, LT, LV, ES, SP, IT, PL, FN, CZ, NW, SW
     }
 }
